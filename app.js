39f04807df39c249eddb51b9f7931e0ca0bee346
Flavio Fontes <fontesflavio1@gmail.com>
2025-01-25 14:17:08 -0300

    adicionado funcionalidade de sorteio

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-let amigos = [ ];
+let amigos = [];
 let text = '';
-
 
 function adicionarAmigo(){
   let novoNome = document.querySelector('input').value;
   if (novoNome == 0){
     alert ('digite um nome valido');
   } else {
+        text = '';
         amigos.push(novoNome);
         for (let i = 0; i < amigos.length; i++) {
-          text += '<li>' + amigos[i];
-          amigos = [];
+          text += '<br>' + amigos[i];
         }
         document.getElementById('listaAmigos').innerHTML = text;
-        limparCampo();
-  }
+      }
+      limparCampo();
+}
+
+
+
+
+function sortearAmigo(){
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    return amigos[indiceAleatorio];
 }
 
 function limparCampo(){
